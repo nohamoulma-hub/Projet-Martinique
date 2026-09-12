d976c626363a532ac365ab84cdf00d83b4ee7b4e
nohamoulma-hub <noham.oulma@gmail.com>
2026-09-12 14:46:34 +0200

    fix: use local photo as cover for Anse Dufour

--- backend/scripts/update_images.py
+++ backend/scripts/update_images.py
@@ -16,12 +16,12 @@ from app.models.point_of_interest import PointOfInterest
 IMAGES = {
     # Photos personnelles locales
     "Anse Noire": "/assets/images/anse_noir/anse_noir_1.jpeg",
+    "Anse Dufour": "/assets/images/anse_dufour/anse_dufour_1.jpeg",
     "Anse Couleuvre": "/assets/images/anse_couleuvre/anse_couleuvre_1.jpeg",
     "Cascade Couleuvre": "/assets/images/photos_vignettes/cascade_couleuvre.JPG",
     # Photos Wikimedia Commons
     "Anse Céron": "https://upload.wikimedia.org/wikipedia/commons/f/fe/Anse_C%C3%A9ron.JPG",
     "Grande Anse des Salines": "https://upload.wikimedia.org/wikipedia/commons/9/95/Martinique-11-Les_Salines_Beach.jpg",
-    "Anse Dufour": "https://upload.wikimedia.org/wikipedia/commons/9/98/Anse_Dufour_-_panoramio.jpg",
     "Plage du Diamant": "https://upload.wikimedia.org/wikipedia/commons/f/f9/Plage_du_Diamant.jpg",
     "Anse à l'Ane": "https://upload.wikimedia.org/wikipedia/commons/d/db/Anse_%C3%A0_l%27%C3%A2ne.jpg",
     "Anse Tartane": "https://upload.wikimedia.org/wikipedia/commons/4/44/Plage_de_Tartane%2C_Martinique.JPG",
